56063fc54826ac4a843cf3cf711eb5bf39dc23d6
айкеновад <@229_13>
2018-08-22 13:45:26 +0600

    -

--- 22.08.2018_recursion_Lesson2/Source.cpp
+++ 22.08.2018_recursion_Lesson2/Source.cpp
@@ -67,58 +67,69 @@ double average(type a[], int n) {
 
 // 2.	Написать функцию, определяющую количество положительных,
 // отрицательных и нулевых элементов передаваемого ей массива.
-
-template<typename type>
-int count_pmz(type arr[], int size, int &pmz) {
+template<typename type> // case 2
+void count_pmz(type arr[], int size, int &p, int &m, int &z) {
 	if (size == 1)
 	{
 		if (arr[size - 1] < 0)
-			return pmz += 100;
+			m++;
 
 		else if (arr[size - 1] > 0)
-			return pmz += 10;
+			p++;
 
 		else if (arr[size - 1] == 0)
-			return pmz += 1;
+			z++;
 	}
 
 	else
 	{
 		if (arr[size - 1] < 0)
-		return pmz + 100 + count_pmz(arr, size - 1, pmz);
-
+		{
+			m++;
+			count_pmz(arr, size - 1, p, m, z);
+		}
 		else if (arr[size - 1] > 0)
-			return pmz + 10 + count_pmz(arr, size - 1, pmz);
-
+		{
+			p++;
+			count_pmz(arr, size - 1, p, m, z);
+		}
 		else if (arr[size - 1] == 0)
-			return pmz + 1 + count_pmz(arr, size - 1, pmz);
+		{
+			z++;
+			count_pmz(arr, size - 1, p, m, z);
+		}
 	}
 }
 
-//// 3.	Написать функцию, определяющую минимум и максимум(значение и номер)
-//// элементов передаваемого ей массива.
-//int position(int arr[], int size, int &min, int &max, int &pos_min, int pos_max) {
-//
-//	if (size == 1)
-//	{
-//		cout << min << " ," << pos_min << endl;
-//		cout << max << " ," << pos_max << endl << endl;
-//		return 1;
-//
-//		if (arr[size - 1] < min)
-//		{
-//			min = arr[size - 1];
-//			pos_min = size - 1;
-//			return position(arr, size - 1, min, max, pos_min, pos_max);
-//		}
-//		if (arr[size - 1] > max)
-//		{
-//			max = arr[size - 1];
-//			pos_max = size - 1;
-//			return position(arr, size - 1, min, max, pos_min, pos_max);
-//		}
-//	}
-//}
+// 3.	Написать функцию, определяющую минимум и максимум(значение и номер)
+// элементов передаваемого ей массива.
+// case 10
+template<typename type>
+void posMinMax(type arr[], int size) {
+	static type min=arr[0];
+	static type max=arr[0];
+	static int pos_min=0;
+	static int pos_max=0;
+
+	if (size == 0)
+	{
+		cout << "min element: " << min << ", its position: " << pos_min << endl;
+		cout << "max element: " << max << ", its position: " << pos_max << endl << endl;
+		return; 
+	}
+
+	if (arr[size - 1] < min)
+	{
+		min = arr[size - 1];
+		pos_min = size - 1;
+	}
+	if (arr[size - 1] > max)
+	{
+		max = arr[size - 1];
+		pos_max = size - 1;
+	}
+	posMinMax(arr, size - 1); // recursion
+}
 void print_char(int n) {
 	if (n == 0)
 		return;
@@ -255,6 +266,37 @@ int main()
 
 		switch (tn)
 		{
+		case 10:
+		{
+			double a[7] = { 1.5, -2.5, -3.5, 4.5, 0, 8.5, 7.5 };
+			int b[] = { 1, 2, 3, 4, 5, 6, 7 };
+
+			posMinMax(b, 7);
+
+			posMinMax(a, 7);
+		}
+		break;
+		case 2:
+		{
+			int k[] = { -1, 0, 3, 0, 5, 6, 7, 0 };
+			int a = 0, b = 0, c = 0;
+			
+			count_pmz(k, 8, a, b, c);
+			
+			cout << "positive elements: " << a << endl;
+			cout << "negative elements: " << b << endl;
+			cout << "zero elements: " << c << endl << endl;
+
+			int a1 = 0, b1 = 0, c1 = 0;
+			double r[7] = { 1.5, -2.5, -3.5, 4.5, 0, 6.5, 7.5 };
+
+			count_pmz(r, 7, a1, b1, c1);
+
+			cout << "positive elements: " << a1 << endl;
+			cout << "negative elements: " << b1 << endl;
+			cout << "zero elements: " << c1 << endl << endl;
+		}
+		break;
 		case 8:
 		{
 			maze_answer(0,0);
@@ -304,27 +346,12 @@ int main()
 
 		}
 		break;
-		case 10:
-		{
-
-		}
-		break;
 		case 1:
 		{
 			double a[8] = { 2, 4.2, 6.4, 8, 10, 12.8, 14.5, 16 };
 
 			cout << average(a, 8) << endl << endl;
 
-		}
-		break;
-		case 2:
-		{
-			double a[7] = { 1.5, -2.5, -3.5, 4.5, 0, 6.5, 7.5 };
-			int b[] = { -1, 0, 3, 0, 5, 6, 0 };
-
-			int pmz = 0;
-
-			cout << count_pmz(b, 7, pmz) << endl << endl;
 		}
 		break;
 		default:
